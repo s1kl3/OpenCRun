b112df1887c3be6d315713057893e6a453acb272
Michele Scandale <scandale@elet.polimi.it>
2013-04-07 18:51:40 +0200

    Fix on llvm_shutdown handling.

--- lib/Core/Platform.cpp
+++ lib/Core/Platform.cpp
@@ -5,16 +5,6 @@
 
 using namespace opencrun;
 
-namespace {
-class ForceInitialization {
-public:
-  ForceInitialization() { llvm::llvm_start_multithreaded(); }
-  ~ForceInitialization() { llvm::llvm_shutdown(); }
-};
-
-ForceInitialization FI;
-}
-
 Platform::Platform() {
   GetCPUDevices(CPUs);
 }
@@ -24,3 +14,15 @@ static llvm::ManagedStatic<Platform> OpenCRunPlat;
 Platform &opencrun::GetOpenCRunPlatform() {
   return *OpenCRunPlat;
 }
+
+static void __init_runtime(void) __attribute__((constructor));
+static void __finalize_runtime(void) __attribute__((destructor));
+
+void __init_runtime(void) {
+  llvm::llvm_start_multithreaded();
+}
+
+void __finalize_runtime(void) {
+  llvm::llvm_shutdown();
+}
+
