663a545f1666a2f28753601fedf526b8c031e38a
Michele Scandale <scandale@elet.polimi.it>
2013-12-08 13:46:11 +0100

    Fixed clRetainDevice, clReleaseDevice implementation in order to support at least proper devices. Sub-devices are not supported yet.

--- tools/libOpenCRun/Device.cpp
+++ tools/libOpenCRun/Device.cpp
@@ -145,12 +145,12 @@ clCreateSubDevices(cl_device_id device,
 
 CL_API_ENTRY cl_int CL_API_CALL
 clRetainDevice(cl_device_id device) CL_API_SUFFIX__VERSION_1_2 {
-  llvm_unreachable("Not yet implemented");
-  return 0;
+  // FIXME: currently there is no support for sub-devices.
+  return CL_SUCCESS;
 }
 
 CL_API_ENTRY cl_int CL_API_CALL
 clReleaseDevice(cl_device_id device) CL_API_SUFFIX__VERSION_1_2 {
-  llvm_unreachable("Not yet implemented");
-  return 0;
+  // FIXME: currently there is no support for sub-devices.
+  return CL_SUCCESS;
 }
